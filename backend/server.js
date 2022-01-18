42ddeb1705e9f3728f34b0dcf17f512bd8749dc3
Elliott Chong <elliottchong16@gmail.com>
2022-01-18 21:21:11 +0800

    fixed google oauth bug origin is undefined

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,9 @@ const io = socketio(server, {wsEngine: 'ws'});
 const PORT = 5001;
 const dotenv = require("dotenv");
 const production = dotenv.config({ path: "../.env" }).parsed.REACT_APP_PRODUCTION==='true';
-const whitelist = ['https://sportbuddy.elliottchong.com', 'http://localhost:3000','https://sportbuddy-elle.netlify.app']
 app.use(
   cors({
-	  origin:function(origin,callback){
-		  if (whitelist.indexOf(origin)!=-1) {
-			  callback(null, true)
-		  }
-		  else {
-			  callback(new Error("Not allowed by CORS elle"))
-		  }
-	  },
+	  origin:production?'https://sportbuddy.elliottchong.com':'http://localhost:3000',
     credentials: true,
   })
 );
